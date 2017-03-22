cbc02822c5c57abb26a55cab7a28508ad1074b0f
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2017-03-22 13:14:46 -0400

    Caching and capistrano fixes

--- Gemfile
+++ Gemfile
@@ -56,4 +56,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :development do
   gem 'capistrano', '~> 3.6'
   gem 'capistrano-rails', '~> 1.2'
+  gem 'capistrano-rvm'
 end
+
+gem "actionpack-page_caching"
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 require 'yaml'
 class ProductsController < ApplicationController
+  caches_page :show, :cache_path => Proc.new { |c| c.params }
+  caches_page :index
+
   def show
     products = YAML.load_file("#{Rails.root.to_s}/config/products.yml")
     @product = products[params[:slug]]
+    redirect_to root_path if @product.nil?
   end
 end
